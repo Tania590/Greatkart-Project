a9f948d4441c677d6a50dc3548bdab0e5a00587f
Tania <taniazuck@gmail.com>
2022-08-05 18:48:32 +0530

    Displaying categories

--- category/admin.py
+++ category/admin.py
@@ -3,5 +3,6 @@ from .models import Category
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
+    list_display = ('title','slug')
 
 admin.site.register(Category, CategoryAdmin)
